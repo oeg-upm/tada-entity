c2b8ec5e11ff891e1bcf41fee681011c7ab1e553
Ahmad Alobaid <ahmad88me@gmail.com>
2019-02-03 12:42:23 +0300

    add readonly views

--- tests/test_readonly_views.py
+++ tests/test_readonly_views.py
@@ -11,3 +11,21 @@ class ReadonyViewsTest(TestCase):
     def test_ent_ann_add(self):
         response = self.client.get(reverse('ent_ann_add'))
         self.assertEqual(response.status_code, 200)
+
+    def test_ent_ann_recompute(self):
+        response = self.client.get(reverse('ent_ann_recompute'))
+        self.assertEqual(response.status_code, 200)
+
+    # def test_ent_ann_raw_results(self):
+    #     response = self.client.get(reverse('ent_ann_raw_results'))
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def test_ent_ann_stored_results(self):
+    #     response = self.client.get(reverse('ent_ann_stored_results'))
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_ent_ann_list(self):
+        response = self.client.get(reverse('ent_ann_list'))
+        self.assertEqual(response.status_code, 200)
+
+
