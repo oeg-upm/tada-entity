656641ef61f5ef0b17aeb8f60d3ba13fbc32789b
Ahmad Alobaid <ahmad88me@gmail.com>
2019-02-06 11:43:15 +0300

    create v2 webcommons models and annotate the columns

--- experiments/webcommons_v2/experiment.py
+++ experiments/webcommons_v2/experiment.py
@@ -28,8 +28,10 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 from tadae.settings import LOG_DIR, UPLOAD_DIR, BASE_DIR
+from tadae.models import AnnRun
 
-
+import annotator
+import commons
 #################################################################################
 #                           JSON to CSV                                         #
 #################################################################################
@@ -38,6 +40,24 @@ from tadae.settings import LOG_DIR, UPLOAD_DIR, BASE_DIR
 import logging
 from commons.logger import set_config
 logger = set_config(logging.getLogger(__name__), logdir=os.path.join(LOG_DIR, 'tadae.log'))
+
+curr_dir = os.path.join(BASE_DIR, 'experiments', 'webcommons_v2')
+meta_dir = os.path.join(curr_dir, "meta.csv")
+
+
+def get_file_and_concept_from_line(line):
+    """
+    get file name without extension and the concept without spaces
+    :param line:
+    :return:
+    """
+    file_name = line[0][:-7]
+    concept = line[1].replace(' ', '_')
+    return file_name, concept
+
+
+def get_csv_file(concept, file_name):
+    return "wcv1_%s_%s.csv" % (concept, file_name)
 
 
 def web_commons_json_table_to_csv(in_file_dir, out_file_dir):
@@ -59,21 +79,51 @@ def web_commons_json_table_to_csv(in_file_dir, out_file_dir):
 
 
 def web_commons_to_csv():
-    curr_dir = os.path.join(BASE_DIR, 'experiments', 'webcommons_v2')
-    f = open(os.path.join(curr_dir, "meta.csv"))
+    f = open(meta_dir)
     reader = csv.reader(f)
     for line in reader:
-        file_name = line[0][:-7]
-        concept = line[1].replace(' ', '_')
+        file_name, concept = get_file_and_concept_from_line(line)
         print file_name, concept
-        csv_file_name = "wcv1_%s_%s.csv" % (concept, file_name)
+        csv_file_name = get_csv_file(concept, file_name)
         output_file = os.path.join(BASE_DIR, UPLOAD_DIR, csv_file_name)
         input_fname = "data/%s.json" % file_name
         input_file = os.path.join(curr_dir, input_fname)
         web_commons_json_table_to_csv(input_file, output_file)
 
 
+def build_empty_models_from_meta():
+    f = open(meta_dir)
+    reader = csv.reader(f)
+    for line in reader:
+        file_name, concept = get_file_and_concept_from_line(line)
+        csv_fname = get_csv_file(concept, file_name)
+        print("csv fname: "+csv_fname)
+        if len(AnnRun.objects.filter(name=csv_fname)) == 0:
+            annotation_run = AnnRun(name=csv_fname, status='Created')
+            annotation_run.save()
+
+
+def annotate_models():
+    anns = AnnRun.objects.filter(status='Created')
+    for ann_run in anns:
+        ann_run.status = "started"
+        ann_run.save()
+        csv_file_dir = os.path.join(proj_path, UPLOAD_DIR, ann_run.name)
+        prefix = "http://dbpedia.org/ontology/"
+        annotator.annotate_csv(ann_run_id=ann_run.id, csv_file_dir=csv_file_dir,
+                               endpoint=commons.ENDPOINT, hierarchy=False, entity_col_id=0, onlyprefix=prefix,
+                               camel_case=False)
+        annotator.dotype(ann_run, commons.ENDPOINT, onlyprefix=None)
+
+
+def workflow():
+    build_empty_models_from_meta()
+    annotate_models()
+
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         if sys.argv[1] == 'tocsv':
             web_commons_to_csv()
+        elif sys.argv[1] == 'annotate':
+            workflow()
