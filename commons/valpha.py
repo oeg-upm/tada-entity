22a3a39127e63af2bf3599bd14a2bdfe8f4836de
Ahmad Alobaid <ahmad88me@gmail.com>
2019-05-01 11:24:38 +0200

    fix all alphas total score

--- commons/valpha.py
+++ commons/valpha.py
@@ -35,8 +35,9 @@ import annotator
 
 #################################################################
 
-
-
+CORRECT = "CORRECT"
+NOTFOUND = "NOTFOUND"
+INCORRECT = "INCORRECT"
 
 # alphas = [0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
 alphas = [0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001, 0.000005, 0.000001]
@@ -85,12 +86,69 @@ def report_alphas(fname, fsid, k, alpha, alphas_fname):
     f.close()
 
 
+# Original (but does not work well if alphas_fname is True )
+# def validate_ent_ann(ent_ann, fsid, ks, correct_type, for_all_alphas, not_found_fname, incorrect_fname,
+#                      correct_fname, alphas_fname):
+#     """
+#     :param ent_ann:
+#     :param fsid:
+#     :param ks: a list of k values that should be sorted in a ascending order
+#     :param correct_type:
+#     :param for_all_alphas: bool (if true, then it will check alphas that yields a good score)
+#     :return:
+#         {
+#             "k_1": bool,
+#             "k_2": bool,
+#             ....
+#         }
+#     """
+#     global alphas
+#     k_id = len(ks)-1  # to start with the largest k
+#     k = ks[k_id]
+#     d = {}
+#     for alpha in alphas:
+#         if for_all_alphas:
+#             k_id = len(ks) - 1  # to start with the largest k
+#             k = ks[k_id]
+#         graph = annotator.load_graph(entity_ann=ent_ann)
+#         results = annotator.score_graph(entity_ann=ent_ann, alpha=alpha, graph=graph, fsid=fsid)[0:k]
+#
+#         if results == []:
+#             for k in ks:
+#                 d[k] = "NOTFOUND"
+#                 report_not_found(fname=ent_ann.ann_run.name, not_found_fname=not_found_fname)
+#             return d
+#
+#         while correct_type in results:
+#             d[k] = "CORRECT"
+#             report_correct(fname=ent_ann.ann_run.name, fsid=fsid, k=k, correct_fname=correct_fname)
+#             if for_all_alphas:
+#                 report_alphas(fname=ent_ann.ann_run.name, fsid=fsid, k=k, alpha=alpha, alphas_fname=alphas_fname)
+#             if k_id == 0:  # no more values of k left
+#                 k_id = -1
+#                 break
+#             else:
+#                 k_id -= 1
+#                 k = ks[k_id]  # new k
+#                 results = results[0:k]
+#
+#         if k_id < 0 and not for_all_alphas:
+#             break
+#
+#     while k_id >= 0:  # there are some values of k that is smaller than k
+#         d[k] = "INCORRECT"
+#         report_incorrect(fname=ent_ann.ann_run.name, fsid=fsid, k=k, incorrect_fname=incorrect_fname)
+#         k_id -= 1
+#         k = ks[k_id]
+#     return d
+
+# Correct even with alphas_fname=True
 def validate_ent_ann(ent_ann, fsid, ks, correct_type, for_all_alphas, not_found_fname, incorrect_fname,
                      correct_fname, alphas_fname):
     """
     :param ent_ann:
     :param fsid:
-    :param ks: a list of k values that should be sorted in a ascending order
+    :param ks: a list of k values (order is not important)
     :param correct_type:
     :param for_all_alphas: bool (if true, then it will check alphas that yields a good score)
     :return:
@@ -101,97 +159,35 @@ def validate_ent_ann(ent_ann, fsid, ks, correct_type, for_all_alphas, not_found_
         }
     """
     global alphas
-    #alphas = [0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
-    k_id = len(ks)-1  # to start with the largest k
-    k = ks[k_id]
+
+    max_k = max(ks)
     d = {}
     for alpha in alphas:
-        if for_all_alphas:
-            k_id = len(ks) - 1  # to start with the largest k
-            k = ks[k_id]
         graph = annotator.load_graph(entity_ann=ent_ann)
-        results = annotator.score_graph(entity_ann=ent_ann, alpha=alpha, graph=graph, fsid=fsid)[0:k]
+        results = annotator.score_graph(entity_ann=ent_ann, alpha=alpha, graph=graph, fsid=fsid)[0:max_k]
 
         if results == []:
             for k in ks:
-                d[k] = "NOTFOUND"
+                d[k] = NOTFOUND
                 report_not_found(fname=ent_ann.ann_run.name, not_found_fname=not_found_fname)
             return d
 
-        while correct_type in results:
-            d[k] = "CORRECT"
-            report_correct(fname=ent_ann.ann_run.name, fsid=fsid, k=k, correct_fname=correct_fname)
-            if for_all_alphas:
-                report_alphas(fname=ent_ann.ann_run.name, fsid=fsid, k=k, alpha=alpha, alphas_fname=alphas_fname)
-            if k_id == 0:  # no more values of k left
-                k_id = -1
-                break
-            else:
-                k_id -= 1
-                k = ks[k_id]  # new k
-                results = results[0:k]
+        for k in ks[::-1]:
+            results = results[0:k]
+            if correct_type in results:
+                # If this is not set before
+                if k not in d or d[k] != CORRECT:
+                    d[k] = CORRECT
+                    report_correct(fname=ent_ann.ann_run.name, fsid=fsid, k=k, correct_fname=correct_fname)
 
-        if k_id < 0 and not for_all_alphas:
-            break
+                if for_all_alphas:
+                    report_alphas(fname=ent_ann.ann_run.name, fsid=fsid, k=k, alpha=alpha, alphas_fname=alphas_fname)
 
-    while k_id >= 0:  # there are some values of k that is smaller than k
-        d[k] = "INCORRECT"
-        report_incorrect(fname=ent_ann.ann_run.name, fsid=fsid, k=k, incorrect_fname=incorrect_fname)
-        k_id -= 1
-        k = ks[k_id]
+    for k in ks:
+        if k not in d or d[k] != CORRECT:
+            d[k] = INCORRECT
+            report_incorrect(fname=ent_ann.ann_run.name, fsid=fsid, k=k, incorrect_fname=incorrect_fname)
     return d
-
-
-# This does not take into account the alphas for different fs
-# def alpha_stat(ks, alphas_fname):
-#     alpha_file_exists = os.path.isfile(alphas_fname)
-#     if not alpha_file_exists:
-#         msg = """
-#             No alpha file is found, to generate it run the application with 'alpha' parameter like that \n
-#             python validation.py alpha
-#         """
-#         print(msg)
-#         return
-#     df = pd.read_csv(alphas_fname, sep='\t')
-#     # print df.columns.values
-#     d = dict()
-#     for k in ks:
-#         df_k = df[df.k==k]
-#         d_count = dict(Counter(df_k['alpha']))
-#         d[k] = d_count
-#     pp = pprint.PrettyPrinter(indent=4)
-#     pp.pprint(d)
-#     print d
-#     plot_alpha_stat(d)
-
-# This does not take into account the alphas for different fs
-# def plot_alpha_stat(d):
-#     import matplotlib
-#     matplotlib.use('TkAgg')
-#     import matplotlib.pyplot as plt
-#
-#     ind = np.arange(len(d[d.keys()[-1]]))  # the x locations for the groups
-#     width = 0.20  # the width of the bars
-#     fig, ax = plt.subplots()
-#
-#     # custom_colors = [ 'royalblue', 'mediumpurple', 'mediumvioletred','hotpink' ,'greenyellow', 'dodgerblue', 'aquamarine' ,'deeppink', 'darkturquoise', 'skyblue']
-#
-#     for idx, k in enumerate(sorted(d.keys())):
-#         vals = []
-#         for a in sorted(d[k].keys()):
-#             vals.append(d[k][a])
-#
-#         _ = ax.bar(ind + width * idx - width/2, vals, width,
-#                    color=cmap(idx*1.0/len(d.keys())),
-#                    #color=custom_colors[idx],
-#                    label="k="+str(k))
-#
-#     ax.set_ylabel('Count')
-#     ax.set_title('Alphas for each k')
-#     ax.set_xticks(ind)
-#     ax.set_xticklabels(tuple(sorted(d[d.keys()[-1]].keys())))
-#     ax.legend()
-#     plt.show()
 
 
 def alpha_stat(ks, alphas_fname):
@@ -283,58 +279,3 @@ def plot_alpha_stat(d):
     ax.legend()
     plt.show()
 
-
-
-# show with plot
-# def plot_alpha_stat(d):
-#     import matplotlib
-#     matplotlib.use('TkAgg')
-#     import matplotlib.pyplot as plt
-#     global alphas
-#     #ind = np.array(alphas)
-#     ind = np.arange(len(alphas))  # the x locations for the groups
-#     width = 0.20  # the width of the bars
-#     fig, ax = plt.subplots()
-#
-#     # custom_colors = [ 'royalblue', 'mediumpurple', 'mediumvioletred','hotpink' ,'greenyellow', 'dodgerblue', 'aquamarine' ,'deeppink', 'darkturquoise', 'skyblue']
-#
-#     for idx, fs in enumerate(sorted(d.keys())):
-#         vals = []
-#         # for a in sorted(d[fs][1].keys()):
-#         #     vals.append(d[fs][1][a])
-#         for a in alphas:
-#             if a in d[fs][1]:
-#                 vals.append(d[fs][1][a])
-#             else:
-#                 vals.append(0)
-#
-#         alphas_as_str = [str(a) for a in alphas]
-#         # _ = ax.bar(alphas_as_str, vals,
-#         #            color=cmap(fs*1.0/len(d.keys())),
-#         #            #color=custom_colors[idx],
-#         #            #label="fs="+str(fs)
-#         #            )
-#         # _ = ax.bar(ind + width * idx - width/2, vals, width,
-#         #            color=cmap(fs*1.0/len(d.keys())),
-#         #            #color=custom_colors[idx],
-#         #            label="fs="+str(fs)
-#         #            )
-#         # _ = ax.bar(ind + width * idx - width/2, vals, width,
-#         #            color=cmap(fs*1.0/len(d.keys())),
-#         #            #color=custom_colors[idx],
-#         #            label="fs="+str(fs)
-#         #            )
-#         print("alphas: "+str(alphas))
-#         print("vals: "+str(vals))
-#         _ = ax.plot(alphas_as_str, vals,
-#                     color=cmap(fs * 1.0 / len(d.keys())),
-#                     )
-#
-#
-#     ax.set_ylabel('Count')
-#     ax.set_title('Alphas for each k')
-#     # ax.set_xticks(ind)
-#     ax.set_xticks(alphas_as_str)
-#     ax.set_xticklabels(tuple(alphas_as_str))
-#     ax.legend()
-#     plt.show()
