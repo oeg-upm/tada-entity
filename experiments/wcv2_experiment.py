d17ccd8d37c6f7406dc02544cbaa597bfeefda01
Ahmad Alobaid <ahmad88me@gmail.com>
2021-05-26 22:45:41 +0200

    add results of wc2 results of title and original cases

--- experiments/wcv2_experiment.py
+++ experiments/wcv2_experiment.py
@@ -1,12 +1,13 @@
 from experiments.experiment_base import ExperimentBase
 import os
 import sys
+from datetime import datetime
 
 
 class WCV2(ExperimentBase):
 
-    def __init__(self, log_fname=None):
-        super().__init__(log_fname)
+    def __init__(self, log_fname=None, title_case=False):
+        super().__init__(log_fname=log_fname, title_case=title_case)
 
     def workflow(self, meta_fdir, subject_col_dir, data_dir, ks):
         d = dict()
@@ -35,9 +36,29 @@ class WCV2(ExperimentBase):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        meta_fdir, sc_dir, data_dir = sys.argv[1:]
-        o = WCV2("wc2-results.csv")
+    if len(sys.argv) >= 4:
+        results_fname_original = "wc2-results.csv"
+        results_fname_title = "wc2-results-title.csv"
+        results_fname = results_fname_original
+        title_case = False
+        meta_fdir, sc_dir, data_dir = sys.argv[1:4]
+        if len(sys.argv) == 5:
+            if sys.argv[4]=="title":
+                print("Title case")
+                results_fname = results_fname_title
+                title_case = True
+            elif sys.argv[4]=="original":
+                print("original case")
+                results_fname = results_fname_original
+                title_case = False
+            else:
+                print("Error: expects the fourth paramerter to either be title or original")
+                raise Exception("Invalid case")
+        start = datetime.now()
+        o = WCV2(results_fname, title_case=title_case)
         o.workflow(meta_fdir=meta_fdir, subject_col_dir=sc_dir, data_dir=data_dir, ks=[1, 3, 5])
+        end = datetime.now()
+        print("time consumed: "+str(end-start))
     else:
         print("Missing arguments: <meta file> <data directory>")
+
